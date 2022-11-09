0b1832bb4ce98ede6c6e751a5219b5f0b5d11fcc
clive2112 <info@gbm.ie>
2022-11-09 16:09:27 +0000

    Add JS Golf card

--- mytest.js
+++ mytest.js
@@ -58,3 +58,25 @@ function lessOrEqual(val) {
 console.log(lessOrEqual(6));
 console.log(lessOrEqual(10));
 console.log(lessOrEqual(9));
+var names = ["Hole in One!", "Eagle", "Birdie", "Par", "Bogey", "Double B", "Fucked"]
+function golfScore (par, strokes) {
+    if (strokes == 1) {
+        return names[0]
+    } else if (strokes <= par - 2) {
+        return names [1]
+    } else if (strokes == par - 1) {
+        return names [2]
+    } else if (strokes == par ) {
+        return names [3]
+    } else if (strokes == par + 1) {
+        return names [4]
+    } else if (strokes == par + 2) {
+        return names [5]
+    } else (strokes >= par + 3) 
+        return names [6]
+    }
+
+
+console.log(golfScore(5,4));
+console.log(golfScore(3,4));
+console.log(golfScore(4,7));
