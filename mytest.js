60b918bfd4c929bd0ccaf525b50e494c18aae407
clive2112 <info@gbm.ie>
2022-11-09 11:29:18 +0000

    Further JS examples

--- mytest.js
+++ mytest.js
@@ -6,3 +6,13 @@ var myStr = "I am \"the walrus\" by a band called \"the beatles\" ";
 console.log(myStr);
 myStr += " Writers credit to: Lennon & McCartney";
 console.log(myStr);
+var myName = "Clive";
+var mySur = "My name is " + myName + " Creevey";
+console.log(mySur);
+var anAdjective = " Trainee Coder!"
+mySur += anAdjective;
+console.log(mySur);
+var myArray = ["Clive", 52];
+console.log(myArray);
+var myData = myArray[1];
+console.log(myData);
