799190248f984a794d6a4fd41f350d36f6583313
clive2112 <info@gbm.ie>
2022-11-09 10:42:19 +0000

    JS example code

--- mytest.js
+++ mytest.js
@@ -1,2 +1,8 @@
 console.log("testing code runner");
-console.log("hello world")
+console.log("hello world");
+var product = 2.0 * 2.5;
+console.log(product);
+var myStr = "I am \"the walrus\" by a band called \"the beatles\" ";
+console.log(myStr);
+myStr += " Writers credit to: Lennon & McCartney";
+console.log(myStr);
